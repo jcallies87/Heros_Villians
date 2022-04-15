c5b023807cfb3043ea599518d801f169ec00222e
Justin Callies <jcallies@gmail.com>
2022-04-15 16:35:00 -0500

    created super_types_list method in views file on super_types app+ updated views file on supers app

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SuperTypeSerializer
+from .models import SuperType
+from supers.models import Supers
+from supers.serializers import SupersSerializer
 # Create your views here.
+@api_view(['GET'])
+def super_types_list(request):
+    super_types = SuperType.objects.all()
+    custom_response_dictionary = {}
+    for super_type in super_types:
+        supers = Supers.objects.filter(super_type_id=super_type_id)
+        supers_serializer = SupersSerializer(supers, many=True)
+        
+
+return Response(custom_response_dictionary)
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SupersSerializer
+from .models import Supers
 # Create your views here.
+@api_view(['GET', 'POST'])
+def products_list(request):
+    
+    if request.method == 'GET':
+        products = Supers.objects.all()
+        serializer = SupersSerializer(products, many= True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = SupersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Supers, pk=pk)
+    if request.method == 'GET':
+        serializer = SupersSerializer(product)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SupersSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
